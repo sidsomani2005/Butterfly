99bd7bcd714d3f1ec76efdd504bfbff3e7757261
Siddhartha (Sid) Somani <163345992+sidsomani2005@users.noreply.github.com>
2024-04-09 02:44:56 -0400

    Add files via upload

---
+++ ButterflyUITests/ButterflyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ButterflyUITestsLaunchTests.swift
+//  ButterflyUITests
+//
+//  Created by Sid Somani on 11/23/23.
+//
+
+import XCTest
+
+final class ButterflyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
